cf06f2eb02ca5c75af287cbd085dd23e306af362
Krunal Patel <98359683+krunalpatel0719@users.noreply.github.com>
2025-02-07 06:55:08 -0600

    Update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,10 +7,6 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
     },
   },
   plugins: [],
